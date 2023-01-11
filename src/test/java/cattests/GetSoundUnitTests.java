1d77cde05c3b1ed0e7c808217f77b6c7011a4d87
mcArtemev <aluygospodin@yandex.ru>
2023-01-11 11:53:39 +0300

    Remove unused imports

--- src/test/java/cattests/GetSoundUnitTests.java
+++ src/test/java/cattests/GetSoundUnitTests.java
@@ -6,7 +6,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
--- src/test/java/felinetests/GetKittensUnitTests.java
+++ src/test/java/felinetests/GetKittensUnitTests.java
@@ -4,8 +4,6 @@ import com.example.Feline;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
